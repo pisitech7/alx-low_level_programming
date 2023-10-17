ebdf2f7de38c933233084fd381b077513fb31673
pisitech7 <omolokevin7@gmail.com>
2023-10-16 19:11:57 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+/**
+ * _strlen - Returns the length of a string.
+ * @s: A pointer to a null-terminated string.
+ *
+ * Description: This function counts the number of characters in the
+ * given string until it reaches the null terminator and returns that count.
+ *
+ * Return: The length of the string as an integer.
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+
+if (s != NULL)
+{
+while (*s != '\0')
+{
+length++;
+s++;
+}
+}
+return (length);
+}
