63c742c1b5efd97ad4358ae534ca436b4f3fb721
pisitech7 <omolokevin7@gmail.com>
2023-10-17 04:22:47 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,14 +14,14 @@
 void print_array(int *a, int n)
 {
 int i;
-if (a != NULL)
-{i
+
 for (i = 0; i < n; i++)
 {
 printf("%d", a[i]);
-if (i < n - 1)
+if (i != (n - 1)
+}
 printf(", ");
 }
-printf("\n");
 }
+printf("\n");
 }
