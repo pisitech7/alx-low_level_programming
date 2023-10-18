89046890f2952163dbf8ae2ca7aa96a120ce9611
pisitech7 <omolokevin7@gmail.com>
2023-10-18 08:27:53 -0700

    Fixed code failure

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b < c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
 
-	if (b > a && a < c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
 
-	if (c > a && a < b)
+	else
 	{
 		largest = c;
 	}
