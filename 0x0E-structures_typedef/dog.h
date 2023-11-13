7532329aaf9aadcab13db743ac70787243a1ff24
pisitech7 <omolokevin7@gmail.com>
2023-11-13 14:01:57 -0800

    a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,33 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure representing information about a dog
+ * @name: Pointer to a character string representing the dog's name
+ * @age: Floating-point number representing the dog's age
+ * @owner: Pointer to a character string representing the dog's owner
+ *
+ * Description: This structure stores information about a dog,
+ * including its name, age, and owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
