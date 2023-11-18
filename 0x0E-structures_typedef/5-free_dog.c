14c78ed806cde058ed51cf6bbd8e3a4248350d21
pisitech7 <omolokevin7@gmail.com>
2023-11-17 22:04:50 -0800

    a function that frees dogs.

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,26 +2,10 @@
 #include <stdlib.h>
 
 /**
- * _strcpy - copies a string to another
- * @dest: destination string
- * @src: source string
- *
- * Return: pointer to dest
- */
-char *_strcpy(char *dest, char *src)
-{	int i = 0;
-
-	while (src[i] != '\0')
-	{
-	dest[i] = src[i];
-	i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-/**
- * free_dog - frees memory allocated for a struct dog
+ * free_dog - free memory allocated for a struct dog
  * @d: struct dog to free
  */
+
 void free_dog(dog_t *d)
 {
 	if (d)
