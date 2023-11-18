3571694b2288f1a0480740b67821f6ad636173c7
pisitech7 <omolokevin7@gmail.com>
2023-11-17 21:51:47 -0800

    corrected a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,14 +9,42 @@
  */
 int _strlen(char *s)
 {
-	int len = 0;
+	int i;
+	i  = 0;
 
-	while (s[len] != '\0')
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+/**
+ * -strcpy - copies the string pointed to by src
+ *  including the terminating null byte (\0)
+ *  to the buffer pointed to by dest
+ *  @dest: pointer to the buffer in which we copy the string
+ *  @src: string to be copied
+ *
+ *  Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
 	{
 		len++;
 	}
-
-	return len;
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
 /**
