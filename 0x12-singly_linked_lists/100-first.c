92293291b2ad23f3369a0512605e2d8eb7239653
pisitech7 <omolokevin7@gmail.com>
2023-11-20 02:36:16 -0800

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-/* Function to print the required text */
-void before_main_execution(void)
+/* Function to print a specific statement before main()*/
+void print_statement(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
+
 /* Main function */
 int main(void)
 {
-	/* Call the function before executing main */
-	before_main_execution();
+	/* Call the function to print the statement before the main function */
+	print_statement();
+	/* Rest of your main code goes here */
 	printf("This is the main function executing.\n");
 	return (0);
 }
+
