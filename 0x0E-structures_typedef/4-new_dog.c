a02ca6ddedd875e4bfd64db23762a6cdcc9562de
pisitech7 <omolokevin7@gmail.com>
2023-11-21 10:09:44 -0800

    a function that prints a struct dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,40 +9,31 @@
  */
 int _strlen(char *s)
 {
-	int i;
-
-	i  = 0;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-
 	return (i);
 }
-/**
- * -strcpy - copies the string pointed to by src
- *  including the terminating null byte (\0)
- *  to the buffer pointed to by dest
- *  @dest: pointer to the buffer in which we copy the string
- *  @src: string to be copied
- *
- *  Return: the pointer to dest
- */
 
+/**
+ * _strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
+	int i;
 
-	len = 0;
-
-	while (src[len] != '\0')
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		len++;
-	}
-	for (i = 0; i < len; i++)
-	{
-		dest[i] = src[i];
+	dest[i] = src[i];
 	}
 	dest[i] = '\0';
 	return (dest);
@@ -63,10 +54,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
-
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-		return (NULL);
+	return (NULL);
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
@@ -76,8 +66,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
-		free(dog);
 		free(dog->name);
+		free(dog);
 		return (NULL);
 	}
 	_strcpy(dog->name, name);
