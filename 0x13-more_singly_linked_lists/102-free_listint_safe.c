8dd0d805696d4a9872a3654ca86a3794b9bce858
pisitech7 <omolokevin7@gmail.com>
2023-11-21 08:48:29 -0800

    a function that reverses a listint_t linked list.

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint_safe - Frees a listint_t list safely
+ * @h: A pointer to a pointer to the head of the list
+ *
+ * Return: The size of the list that was freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current = *h, *temp;
+	size_t count = 0;
+
+	while (current != NULL && current > current->next)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+		count++;
+	}
+	*h = NULL;
+	return (count);
+}
