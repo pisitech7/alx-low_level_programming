c417f6768fc5dd11fc93fd8cf0938aeb047b1129
pisitech7 <omolokevin7@gmail.com>
2023-11-21 11:08:54 -0800

    a function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,42 @@
-#include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
 
-void print_all(const char * const format, ...) 
+void print_all(const char * const format, ...)
 {
 	va_list args;
-	char *str_arg;
-	int int_arg;
-	float float_arg;
-	int i = 0;
-	char c;
+	unsigned int i = 0;
+	char *str;
 
 	va_start(args, format);
 
-	while (format && format[i]) 
+	while (format && format[i])
 	{
-	c = format[i];
-	switch (c) 
+	switch (format[i])
 	{
 	case 'c':
-	int_arg = va_arg(args, int);
-	printf("%c", int_arg);
-	break;
+	printf("%c", va_arg(args, int));
+		break;
 	case 'i':
-	int_arg = va_arg(args, int);
-	printf("%d", int_arg);
-	break;
+	printf("%d", va_arg(args, int));
+		break;
 	case 'f':
-	float_arg = (float)va_arg(args, double);
-	printf("%f", float_arg);
-	break;
+	printf("%f", (float)va_arg(args, double));
+		break;
 	case 's':
-	str_arg = va_arg(args, char*);
-	if (str_arg == NULL) 
-	{
+	str = va_arg(args, char *);
+	if (str == NULL)
 	printf("(nil)");
+	else
+	printf("%s", str);
+		break;
+	default:
 	break;
 	}
-	printf("%s", str_arg);
-	break;
- 	default:
-	break;
-	}
-	if ((format[i + 1] != '\0') && (c == 'c' || c == 'i' || c == 'f' || c == 's'))
- 	printf(", ");
-	i++;
+	if ((format[i + 1] != '\0') && (format[i + 1] == 'c' || format[i + 1] == 'i' ||
+           format[i + 1] == 'f' || format[i + 1] == 's'))
+	printf(", ");
+		i++;
 	}
 	printf("\n");
 	va_end(args);
